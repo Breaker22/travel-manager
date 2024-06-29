57e8fe9304d2e88f98bcc28a83a0008da126a624
Breaker22 <gastyg16@gmail.com>
2024-06-29 19:03:33 -0300

    Add get-best-path service

--- src/main/java/ar/com/travel/manager/controller/PathController.java
+++ src/main/java/ar/com/travel/manager/controller/PathController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import ar.com.travel.manager.model.exception.PathServiceException;
 import ar.com.travel.manager.model.request.PathRequest;
 import ar.com.travel.manager.model.response.GenericErrorResponse;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/paths")
@@ -28,6 +30,12 @@ public class PathController {
 	public ResponseEntity<?> createPath(@Valid @RequestBody PathRequest request) {
 		pathInterface.createPath(request);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@GetMapping("/{sourceId}/{destinationId}")
+	public ResponseEntity<?> getBestPath(@PathParam(value = "sourceId") Long sourceId,
+			@PathParam(value = "destinationId") Long destinationId) {
+		return ResponseEntity.ok().body(pathInterface.getBestPath(sourceId, destinationId));
 	}
 
 	@ExceptionHandler(PathServiceException.class)
--- src/main/java/ar/com/travel/manager/interfaces/path/PathInterface.java
+++ src/main/java/ar/com/travel/manager/interfaces/path/PathInterface.java
@@ -2,9 +2,10 @@ package ar.com.travel.manager.interfaces.path;
 
 import ar.com.travel.manager.model.exception.PathServiceException;
 import ar.com.travel.manager.model.request.PathRequest;
+import ar.com.travel.manager.model.response.path.PathResponse;
 
 public interface PathInterface {
-	
+
 	/**
 	 * Crea el camino
 	 * 
@@ -12,5 +13,14 @@ public interface PathInterface {
 	 * @throws PathServiceException
 	 */
 	public void createPath(PathRequest request);
+
+	/**
+	 * Busca el mejor precio de un camino
+	 * 
+	 * @param sourceId      - id de origen
+	 * @param destinationId - id destino
+	 * @return response del mejor camino
+	 */
+	public PathResponse getBestPath(Long sourceId, Long destinationId);
 
 }
